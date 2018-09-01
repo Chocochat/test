336acf18794bea471bcef795a97a71e366ecad59
ramasas1 <>
2018-09-02 09:28:18 +1000

    Initial commit

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -5,7 +5,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.model.GatewayResponse;
 import com.aws.codestar.projecttemplates.service.SortingService;
+import com.aws.codestar.projecttemplates.service.SortingServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 
 /**
  * Handler for requests to Lambda function. test
@@ -13,15 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
-    final SortingService sortingService;
-
-    public HelloWorldHandler(SortingService sortingService) {
-        this.sortingService = sortingService;
-    }
 
 
     public Object handleRequest(Object object, Context context) {
         log.error("inputRequest: {}", object.toString());
+        SortingService sortingService = new SortingServiceImpl();
         GatewayResponse gatewayResponse =  sortingService.getSortedData(context.getAwsRequestId());
 
 
