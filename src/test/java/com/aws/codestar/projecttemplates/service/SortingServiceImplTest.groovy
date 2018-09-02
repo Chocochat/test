6f3eca336f219c6b6687cd233c0893747a404dad
ramasas1 <>
2018-09-03 01:28:36 +1000

    testing

--- src/test/java/com/aws/codestar/projecttemplates/service/SortingServiceImplTest.groovy
+++ src/test/java/com/aws/codestar/projecttemplates/service/SortingServiceImplTest.groovy
@@ -33,17 +33,12 @@ class SortingServiceImplTest extends Specification {
     def "test getSortedData for malformed or error data "() {
         given: 'valid request'
         def sortingServiceImpl = Spy(SortingServiceImpl, constructorArgs: [])
-        def reader = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/inputdata.json"), "UTF-8"))
-        def request = new JsonParser().parse(reader)
-        def responseReader = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/responsedata.json"), "UTF-8"))
-        def response = new JsonParser().parse(responseReader)
         when: 'get external data and do comparision'
         def gatewayResponse = sortingServiceImpl.getSortedData("4567-5467")
         then: 'return valid response'
         1 * sortingServiceImpl.getHttpUtils() >> httpUtils
         1 * httpUtils.getResponse(_, _) >> responseEntity
         2 * responseEntity.getStatusCodeValue() >> 400
-//        1 * responseEntity.getBody() >> request.toString()
 
         def exception = thrown(ErrorGatewayResponse)
         exception.statusCode == 400
